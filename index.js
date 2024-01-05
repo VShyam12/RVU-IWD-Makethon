72c3ba02ad102e7e6e763ab7d3776d51b9b07b66
VShyam12 <155656654+VShyam12@users.noreply.github.com>
2024-01-06 01:07:45 +0530

    Add files via upload
    These files contain the code to the resume which I have developed for myself. This project was assigned to me in the IWD Makeathon.

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    btn.style.display='none';
+
+    const msg1 = document.getElementById('msg1');
+    msg1.style.display='block';
+});
